52668a84abc252d526c6771be7a7e81feb47d301
degen <degenhinata@gmail.com>
2025-05-24 02:17:38 +0100

    starting Page routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import './index.css'
 import { Header } from "./components/Header"
 import { FreeDownload } from './components/Section'
-import { Articles } from './components/article'
+import { Articles } from './components/Article'
+import { Layout } from './components/Layout'
 
 function App() {
   
 
   return (
     <>
-      <Header />
-      <FreeDownload />
-      <Articles />
+      <Layout>
+        <FreeDownload />
+        <Articles />
+      </Layout>
     </>
   )
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,67 @@
-// export function Layout(){
-    
-   // const header = 
+import { useState } from 'react'
+import logo from '../assets/kloudaa logo.png'
+import { FaBars   } from 'react-icons/fa'
 
-    //return(
-      //  {header}
-            // <main>
 
-          //   </main>
-       // {footer}
+export function Layout(props){
 
+    const { children } = props
+
+    const [show,setShow] = useState(false)
+
+     function onShowclick ()  {
+             if ( show == false) {
+                 setShow(true)
+             } else {setShow(false)};
+         }
+
+   const header = (
+         <header className='header'>
+                <div className='logoHolder'>
+                     <img src={logo}  className='klogo'/> 
+                </div> 
+                { show &&
+                 <nav className='NavOptions'>
+                     <ul>About</ul>
+                     <ul>Privacy Policy</ul>
+                     <ul> Terms and Conditions</ul>
+                 </nav>
+     }
+     
+                 <nav className='navOptions'>
+                     <ul>About</ul>
+                     <ul>Privacy Policy</ul>
+                     <ul> Terms and Conditions</ul>
+                 </nav>
+                 <FaBars 
+                     onClick={onShowclick}
+                     className='optionMenu'
+                 />
+                 <button className='regbtn'> Web App Coming Soon</button>
+                 
+             </header>
+         
+   );
+
+   const footer = (
+     <div>
+        <footer>
             
-    //)
-//}
+        </footer>
+
+     </div>
+   )
+
+return(
+    <>
+         {header}
+            <main>
+                {children}
+            </main>
+       {footer}
+
+    </>
+      
+            
+    )
+}
